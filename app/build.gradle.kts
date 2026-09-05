b59ca804b75e7465506971575d5c03c51248218e
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-05 07:38:57 -0400

    chore: bump version to 0.8.2 (versionCode 25) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "dev.chungjungsoo.gptmobile.improved"
         minSdk = 31
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.8.1"
+        versionCode = 25
+        versionName = "0.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

62a68a06d4b1180e3872fccaa35675d9c4fa1e44
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-05 00:38:54 -0400

    fix(di): provide missing V1 and V2 Room database and DAO bindings in DatabaseModule

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/di/DatabaseModule.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/di/DatabaseModule.kt
@@ -7,17 +7,62 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dev.chungjungsoo.gptmobile.data.database.ChatDatabase
+import dev.chungjungsoo.gptmobile.data.database.ChatDatabaseMigrations
 import dev.chungjungsoo.gptmobile.data.database.ChatDatabaseV2
 import dev.chungjungsoo.gptmobile.data.database.ChatDatabaseV2Migrations
+import dev.chungjungsoo.gptmobile.data.database.dao.AgentPersistenceDao
+import dev.chungjungsoo.gptmobile.data.database.dao.AgentRunDao
+import dev.chungjungsoo.gptmobile.data.database.dao.ChatPlatformModelV2Dao
+import dev.chungjungsoo.gptmobile.data.database.dao.ChatRoomDao
 import dev.chungjungsoo.gptmobile.data.database.dao.ChatRoomV2Dao
+import dev.chungjungsoo.gptmobile.data.database.dao.LocalModelDao
+import dev.chungjungsoo.gptmobile.data.database.dao.MessageDao
 import dev.chungjungsoo.gptmobile.data.database.dao.MessageV2Dao
+import dev.chungjungsoo.gptmobile.data.database.dao.PlatformV2Dao
+import dev.chungjungsoo.gptmobile.data.database.dao.ToolConnectionDao
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
-    private const val DATABASE_NAME = "chat_database"
+    private const val V1_DATABASE_NAME = "chat"
+    private const val V2_DATABASE_NAME = "chat_database"
+
+    @Provides
+    @Singleton
+    fun provideChatDatabase(
+        @ApplicationContext context: Context
+    ): ChatDatabase {
+        return Room.databaseBuilder(
+            context,
+            ChatDatabase::class.java,
+            V1_DATABASE_NAME
+        )
+            .addMigrations(
+                ChatDatabaseMigrations.MIGRATION_1_2,
+                ChatDatabaseMigrations.MIGRATION_2_3,
+                ChatDatabaseMigrations.MIGRATION_3_4,
+                ChatDatabaseMigrations.MIGRATION_4_5,
+                ChatDatabaseMigrations.MIGRATION_5_6,
+                ChatDatabaseMigrations.MIGRATION_6_7,
+                ChatDatabaseMigrations.MIGRATION_7_8,
+                ChatDatabaseMigrations.MIGRATION_8_9
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun provideChatRoomDao(database: ChatDatabase): ChatRoomDao {
+        return database.chatRoomDao()
+    }
+
+    @Provides
+    fun provideMessageDao(database: ChatDatabase): MessageDao {
+        return database.messageDao()
+    }
 
     @Provides
     @Singleton
@@ -27,7 +72,7 @@ object DatabaseModule {
         return Room.databaseBuilder(
             context,
             ChatDatabaseV2::class.java,
-            DATABASE_NAME
+            V2_DATABASE_NAME
         )
             .addMigrations(
                 ChatDatabaseV2Migrations.MIGRATION_10_11,
@@ -46,5 +91,35 @@ object DatabaseModule {
     @Provides
     fun provideMessageV2Dao(database: ChatDatabaseV2): MessageV2Dao {
         return database.messageDao()
+    }
+
+    @Provides
+    fun providePlatformV2Dao(database: ChatDatabaseV2): PlatformV2Dao {
+        return database.platformDao()
+    }
+
+    @Provides
+    fun provideChatPlatformModelV2Dao(database: ChatDatabaseV2): ChatPlatformModelV2Dao {
+        return database.chatPlatformModelDao()
+    }
+
+    @Provides
+    fun provideAgentRunDao(database: ChatDatabaseV2): AgentRunDao {
+        return database.agentRunDao()
+    }
+
+    @Provides
+    fun provideAgentPersistenceDao(database: ChatDatabaseV2): AgentPersistenceDao {
+        return database.agentPersistenceDao()
+    }
+
+    @Provides
+    fun provideToolConnectionDao(database: ChatDatabaseV2): ToolConnectionDao {
+        return database.toolConnectionDao()
+    }
+
+    @Provides
+    fun provideLocalModelDao(database: ChatDatabaseV2): LocalModelDao {
+        return database.localModelDao()
     }
 }

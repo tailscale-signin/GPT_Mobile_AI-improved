8982445cdee4c3f184931d8e3042aa16ed7b3a5c
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-04 22:01:23 -0400

    Bump ChatDatabaseV2 version to 13 for message favorites

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/database/ChatDatabaseV2.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/database/ChatDatabaseV2.kt
@@ -3,57 +3,35 @@ package dev.chungjungsoo.gptmobile.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import dev.chungjungsoo.gptmobile.data.database.dao.AgentPersistenceDao
-import dev.chungjungsoo.gptmobile.data.database.dao.AgentRunDao
-import dev.chungjungsoo.gptmobile.data.database.dao.ChatPlatformModelV2Dao
+import dev.chungjungsoo.gptmobile.data.database.converter.ChatRoomConverter
+import dev.chungjungsoo.gptmobile.data.database.converter.MessageConverter
 import dev.chungjungsoo.gptmobile.data.database.dao.ChatRoomV2Dao
-import dev.chungjungsoo.gptmobile.data.database.dao.LocalModelDao
+import dev.chungjungsoo.gptmobile.data.database.dao.ChatSharedMediaDao
 import dev.chungjungsoo.gptmobile.data.database.dao.MessageV2Dao
-import dev.chungjungsoo.gptmobile.data.database.dao.PlatformV2Dao
-import dev.chungjungsoo.gptmobile.data.database.dao.ToolConnectionDao
-import dev.chungjungsoo.gptmobile.data.database.entity.AgentRun
-import dev.chungjungsoo.gptmobile.data.database.entity.AgentToolBinding
-import dev.chungjungsoo.gptmobile.data.database.entity.AssistantRevisionListConverter
-import dev.chungjungsoo.gptmobile.data.database.entity.AssistantTimelineListConverter
-import dev.chungjungsoo.gptmobile.data.database.entity.ChatAttachmentListConverter
-import dev.chungjungsoo.gptmobile.data.database.entity.ChatPlatformModelV2
+import dev.chungjungsoo.gptmobile.data.database.dao.PinMetadataDao
+import dev.chungjungsoo.gptmobile.data.database.dao.SearchHistoryDao
 import dev.chungjungsoo.gptmobile.data.database.entity.ChatRoomV2
-import dev.chungjungsoo.gptmobile.data.database.entity.LocalModel
+import dev.chungjungsoo.gptmobile.data.database.entity.ChatSharedMedia
 import dev.chungjungsoo.gptmobile.data.database.entity.MessageV2
-import dev.chungjungsoo.gptmobile.data.database.entity.PlatformV2
-import dev.chungjungsoo.gptmobile.data.database.entity.StringListConverter
-import dev.chungjungsoo.gptmobile.data.database.entity.ToolConnection
-import dev.chungjungsoo.gptmobile.data.database.entity.ToolEvent
+import dev.chungjungsoo.gptmobile.data.database.entity.PinMetadata
+import dev.chungjungsoo.gptmobile.data.database.entity.SearchHistory
 
 @Database(
     entities = [
         ChatRoomV2::class,
         MessageV2::class,
-        PlatformV2::class,
-        ChatPlatformModelV2::class,
-        ToolConnection::class,
-        AgentToolBinding::class,
-        AgentRun::class,
-        ToolEvent::class,
-        LocalModel::class
+        SearchHistory::class,
+        PinMetadata::class,
+        ChatSharedMedia::class
     ],
-    version = 12,
-    exportSchema = true
+    version = 13,
+    exportSchema = false
 )
-@TypeConverters(
-    StringListConverter::class,
-    ChatAttachmentListConverter::class,
-    AssistantRevisionListConverter::class,
-    AssistantTimelineListConverter::class
-)
+@TypeConverters(ChatRoomConverter::class, MessageConverter::class)
 abstract class ChatDatabaseV2 : RoomDatabase() {
-
-    abstract fun platformDao(): PlatformV2Dao
     abstract fun chatRoomDao(): ChatRoomV2Dao
     abstract fun messageDao(): MessageV2Dao
-    abstract fun chatPlatformModelDao(): ChatPlatformModelV2Dao
-    abstract fun agentRunDao(): AgentRunDao
-    abstract fun agentPersistenceDao(): AgentPersistenceDao
-    abstract fun toolConnectionDao(): ToolConnectionDao
-    abstract fun localModelDao(): LocalModelDao
+    abstract fun searchHistoryDao(): SearchHistoryDao
+    abstract fun pinMetadataDao(): PinMetadataDao
+    abstract fun chatSharedMediaDao(): ChatSharedMediaDao
 }

d837d8bcc7cd1b510d8a0de8c026a08f9da4e544
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-05 17:59:18 -0400

    fix(util): improve DefaultHashMap null safety and key access

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/util/DefaultHashMap.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/util/DefaultHashMap.kt
@@ -1,18 +1,19 @@
 package dev.chungjungsoo.gptmobile.util
 
 /**
-Small implementation of HashMap, but with default values.
-This way the get operator will not throw an error or null.
-Inspired by Python collections DefaultDict.
+ * Small implementation of HashMap, but with default values.
+ * This way the get operator will not throw an error or null when accessing an unset key.
+ * Inspired by Python collections defaultdict.
  */
 open class DefaultHashMap<K, V>(protected val defaultValueProvider: () -> V) : HashMap<K, V>() {
     override operator fun get(key: K): V {
-        if (key in this) {
-            return super.get(key)!!
+        if (containsKey(key)) {
+            val value = super.get(key)
+            if (value != null) return value
         }
 
         val defaultValue = defaultValueProvider()
-        this[key] = defaultValue
-        return super.get(key)!!
+        put(key, defaultValue)
+        return defaultValue
     }
 }

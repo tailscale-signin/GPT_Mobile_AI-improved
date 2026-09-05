e41ce0a68ce39a81a59e84dec28f40937efcbbfe
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-05 19:01:28 -0400

    refactor: migrate deprecated TabRow to PrimaryTabRow in HomeScreen

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeScreen.kt
@@ -45,9 +45,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -183,7 +183,7 @@ fun HomeScreen(
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             if (!chatListState.isSelectionMode && !chatListState.isSearchMode) {
-                TabRow(
+                PrimaryTabRow(
                     selectedTabIndex = currentTab.ordinal,
                     containerColor = MaterialTheme.colorScheme.background
                 ) {

e2033d4b78b086c04d25f5dc0844e3ab1a166e7d
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-06 11:58:39 -0400

    fix: bind ChatRepositoryImpl with @Binds in ChatRepositoryModule

--- app/src/main/kotlin/dev/melo/gptmobile/improved/di/ChatRepositoryModule.kt
+++ app/src/main/kotlin/dev/melo/gptmobile/improved/di/ChatRepositoryModule.kt
@@ -1,47 +1,20 @@
 package dev.melo.gptmobile.improved.di
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.melo.gptmobile.improved.data.database.dao.ChatPlatformModelV2Dao
-import dev.melo.gptmobile.improved.data.database.dao.ChatV2Dao
-import dev.melo.gptmobile.improved.data.database.dao.PlatformV2Dao
-import dev.melo.gptmobile.improved.data.network.AnthropicAPI
-import dev.melo.gptmobile.improved.data.network.GoogleAPI
-import dev.melo.gptmobile.improved.data.network.GroqAPI
-import dev.melo.gptmobile.improved.data.network.OpenAIAPI
 import dev.melo.gptmobile.improved.data.repository.ChatRepository
 import dev.melo.gptmobile.improved.data.repository.ChatRepositoryImpl
-import dev.melo.gptmobile.improved.data.repository.LocalModelRepository
-import dev.melo.gptmobile.improved.data.security.SecretVault
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object ChatRepositoryModule {
+abstract class ChatRepositoryModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideChatRepository(
-        chatV2Dao: ChatV2Dao,
-        platformV2Dao: PlatformV2Dao,
-        chatPlatformModelV2Dao: ChatPlatformModelV2Dao,
-        openAIAPI: OpenAIAPI,
-        groqAPI: GroqAPI,
-        anthropicAPI: AnthropicAPI,
-        googleAPI: GoogleAPI,
-        localModelRepository: LocalModelRepository,
-        secretVault: SecretVault
-    ): ChatRepository = ChatRepositoryImpl(
-        chatV2Dao = chatV2Dao,
-        platformV2Dao = platformV2Dao,
-        chatPlatformModelV2Dao = chatPlatformModelV2Dao,
-        openAIAPI = openAIAPI,
-        groqAPI = groqAPI,
-        anthropicAPI = anthropicAPI,
-        googleAPI = googleAPI,
-        localModelRepository = localModelRepository,
-        secretVault = secretVault
-    )
+    abstract fun bindChatRepository(
+        impl: ChatRepositoryImpl
+    ): ChatRepository
 }

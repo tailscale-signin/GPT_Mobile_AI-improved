3d5b1e256d2156f75c9d6c5f7669b2a939cb2f1f
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-06 13:20:04 -0400

    chore: remove unused PlatformName.kt utility

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/util/PlatformName.kt
+++
@@ -1,5 +0,0 @@
-package dev.chungjungsoo.gptmobile.util
-
-import dev.chungjungsoo.gptmobile.data.database.entity.PlatformV2
-
-fun List<PlatformV2>.getPlatformName(uid: String): String = this.find { it.uid == uid }?.name ?: "Unknown"

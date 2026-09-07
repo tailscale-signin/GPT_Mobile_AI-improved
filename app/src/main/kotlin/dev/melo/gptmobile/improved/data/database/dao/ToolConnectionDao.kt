db3a4e4301b3d8076c1d3c841766e30d3ba0047c
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-07 08:14:16 -0400

    clean(dao): remove unused AgentToolBindingWithConnection import from ToolConnectionDao

--- app/src/main/kotlin/dev/melo/gptmobile/improved/data/database/dao/ToolConnectionDao.kt
+++ app/src/main/kotlin/dev/melo/gptmobile/improved/data/database/dao/ToolConnectionDao.kt
@@ -7,7 +7,6 @@ import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Upsert
 import dev.melo.gptmobile.improved.data.database.entity.AgentToolBinding
-import dev.melo.gptmobile.improved.data.database.entity.AgentToolBindingWithConnection
 import dev.melo.gptmobile.improved.data.database.entity.BuiltInAgentTool
 import dev.melo.gptmobile.improved.data.database.entity.ToolConnection
 import dev.melo.gptmobile.improved.data.database.entity.ToolConnectionType

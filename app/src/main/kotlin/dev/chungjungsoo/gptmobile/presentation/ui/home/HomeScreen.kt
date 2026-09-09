d266cae727fbe5290f5374b81f59bf2222f54c01
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-09 13:15:39 -0400

    Use getChatRoom in onViewInChat in HomeScreen

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeScreen.kt
@@ -358,10 +358,12 @@ fun HomeScreen(
                     homeViewModel.assignFavoriteMessageGroup(detailMessage.id, group)
                 },
                 onViewInChat = {
-                    val targetChat = chatListState.chats.find { it.id == detailMessage.chatId }
+                    val targetMessage = detailMessage
                     selectedDetailMessage = null
-                    if (targetChat != null) {
-                        onExistingChatClick(targetChat, detailMessage.id)
+                    homeViewModel.getChatRoom(targetMessage.chatId) { targetChat ->
+                        if (targetChat != null) {
+                            onExistingChatClick(targetChat, targetMessage.id)
+                        }
                     }
                 },
                 onUnfavorite = {

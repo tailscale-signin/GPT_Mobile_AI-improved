7c8cd73c180da0ffc6f5ad1081fe03e316977980
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-11 22:49:41 -0400

    fix(di): add explicit @Target and RUNTIME retention to DeviceRamGb qualifier

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/di/DeviceRamGb.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/di/DeviceRamGb.kt
@@ -3,5 +3,11 @@ package dev.chungjungsoo.gptmobile.di
 import javax.inject.Qualifier
 
 @Qualifier
-@Retention(AnnotationRetention.BINARY)
+@Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER
+)
 annotation class DeviceRamGb

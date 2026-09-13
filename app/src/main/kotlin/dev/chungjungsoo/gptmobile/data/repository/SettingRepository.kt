b77a6260c4091a267314ef7e958e3cfd44971b33
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-12 22:59:29 -0400

    Add get/update LocalRuntimeBackend to SettingRepository

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/repository/SettingRepository.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/repository/SettingRepository.kt
@@ -3,6 +3,7 @@ package dev.chungjungsoo.gptmobile.data.repository
 import dev.chungjungsoo.gptmobile.data.database.entity.PlatformV2
 import dev.chungjungsoo.gptmobile.data.dto.Platform
 import dev.chungjungsoo.gptmobile.data.dto.ThemeSetting
+import dev.chungjungsoo.gptmobile.data.model.LocalRuntimeBackend
 import kotlinx.coroutines.flow.Flow
 
 interface SettingRepository {
@@ -11,6 +12,8 @@ interface SettingRepository {
     fun observePlatformV2s(): Flow<List<PlatformV2>>
     fun observePlatformV2ByUid(uid: String): Flow<PlatformV2?>
     suspend fun fetchThemes(): ThemeSetting
+    suspend fun getLocalRuntimeBackend(): LocalRuntimeBackend
+    suspend fun updateLocalRuntimeBackend(backend: LocalRuntimeBackend)
     suspend fun migrateToPlatformV2()
     suspend fun migrateSecrets(): List<SecretMigrationError>
     suspend fun updatePlatforms(platforms: List<Platform>)

c69c1fd87c215fde9af7d3d469ffc3c4efd98106
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-13 22:51:48 -0400

    fix(build): allow hilt gradle plugin resolution from mavenCentral with resolutionStrategy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,15 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.google.dagger.hilt.android") {
+                useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version ?: "2.59.2"}")
+            }
+        }
     }
 }
 dependencyResolutionManagement {

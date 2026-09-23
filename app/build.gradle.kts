627a08f9d3b5ede0b95e7b94675a4e9574be9460
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-22 20:16:43 -0400

    build: sync version to 0.9.7.0 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "dev.melo.gptmobile.improved"
         minSdk = 31
         targetSdk = 36
-        versionCode = 61
-        versionName = "0.9.6.2"
+        versionCode = 62
+        versionName = "0.9.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

65d3a9ef637973db2c08aea1541ef6d2ad99b8ee
tailscale-signin <github-tailscale@art3d.xyz>
2026-09-25 10:17:46 -0400

    chore: bump version to 0.9.11.0 (versionCode 66)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "dev.melo.gptmobile.improved"
         minSdk = 31
         targetSdk = 36
-        versionCode = 65
-        versionName = "0.9.9.0"
+        versionCode = 66
+        versionName = "0.9.11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
